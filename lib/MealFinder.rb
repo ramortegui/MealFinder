33360e323f73ee81a258a4e4c11a9dbee20a3eac
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-01-20 19:46:54 -0700

    Add basic object architecture.

--- lib/MealFinder.rb
+++ lib/MealFinder.rb
@@ -1,3 +1,40 @@
 class MealFinder
+  attr_accessor :restaurants
+  require 'set'
 
+  def initialize()
+    @restaurants = SortedSet.new
+  end
+
+  def addRestaurant(restaurant)
+    @restaurants << restaurant
+  end
+end
+
+class Restaurant
+  attr_accessor :name,:rating
+  include Comparable
+
+  def initialize(name, rating)
+    @name = name
+    @rating = rating
+    @menus = Hash.new
+  end
+
+ def <=>( other)
+   @rating <=> other.rating
+ end
+
+  def addMenu(menu)
+    @menus[menu.name] = menu
+  end
+
+end
+
+class Menu
+  attr_accessor :name, :quantity
+  def initialize(name, quantity)
+    @name = name
+    @quantity = quantity
+  end
 end
