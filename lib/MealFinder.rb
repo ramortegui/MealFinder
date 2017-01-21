d51e3d4cf6073980164f36e8255c96dde5f0acd4
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-01-21 01:01:59 -0700

    Add method to find meals in restaurants.  Add base sample.

--- lib/MealFinder.rb
+++ lib/MealFinder.rb
@@ -9,32 +9,76 @@ class MealFinder
   def addRestaurant(restaurant)
     @restaurants << restaurant
   end
+
+  #receive a hash with the requirements
+  def finder(order_menu)
+    result = {}
+    menus = order_menu.menus
+    @restaurants.reverse_each do |restaurant|
+      restaurant_temp = restaurant
+      menus.keys.each do |name|
+        if ( restaurant.menus[name] && restaurant_temp.menus[name].quantity > 0 )
+          if ( menus[name].quantity <= restaurant_temp.menus[name].quantity ) 
+            result[restaurant_temp.name] = {} unless result[restaurant_temp.name]
+            result[restaurant_temp.name][name] = menus[name].quantity
+            restaurant_temp.menus[name].remove_qty(menus[name].quantity)
+          else
+            result[restaurant_temp.name] = {} unless result[restaurant_temp.name]
+            result[restaurant_temp.name][name] = restaurant_temp.menus[name].quantity
+            menus[name].remove_qty(restaurant_temp.menus[name].quantity)
+          end
+        end
+      end
+    end
+    result
+  end
 end
 
 class Restaurant
-  attr_accessor :name,:rating
+  attr_reader :name,:rating,:meals_qty, :menus
+
   include Comparable
 
-  def initialize(name, rating)
+  def initialize(name, rating, meals_qty)
     @name = name
     @rating = rating
     @menus = Hash.new
-  end
+    @menus["other"] = Menu.new("other", meals_qty)
 
- def <=>( other)
-   @rating <=> other.rating
- end
+  end
 
   def addMenu(menu)
     @menus[menu.name] = menu
+    @menus["other"].remove_qty(menu.quantity)
+  end
+
+  def <=>( other)
+   @rating <=> other.rating
   end
 
 end
 
 class Menu
-  attr_accessor :name, :quantity
+  attr_reader :name, :quantity
   def initialize(name, quantity)
     @name = name
     @quantity = quantity
+  end
+
+  def remove_qty(quantity)
+    @quantity -= quantity
+  end
+end
+
+class OrderMenu
+  attr_reader :menus
+  def initialize(quantity)
+    @menus = {}
+    @menus["other"] = Menu.new("other", quantity)
+  end
+
+  def addMenu(menu)
+    @menus[menu.name] = menu
+    @menus["other"].remove_qty(menu.quantity)
   end
 end
--- tests/test_MealFinder.rb
+++ tests/test_MealFinder.rb
@@ -8,5 +8,31 @@ class TestMealFinder < MiniTest::Unit::TestCase
     m = MealFinder.new
     assert_equal(MealFinder,m.class)
   end
-  
+
+  def test_finder
+    m = MealFinder.new
+    
+    r = Restaurant.new("A",5,40)
+    r.addMenu(Menu.new("vegetarians",4))
+    m.addRestaurant(r)
+
+    r = Restaurant.new("B",3,100)
+    r.addMenu(Menu.new("vegetarians",20))
+    r.addMenu(Menu.new("gluten free",20))
+    m.addRestaurant(r)
+
+    om = OrderMenu.new(50)
+    om.addMenu(Menu.new("vegetarians",5))
+    om.addMenu(Menu.new("gluten free",7))
+
+    res =  m.finder(om)
+
+    assert_equal(res["A"]["vegetarians"],4)
+    assert_equal(res["A"]["other"],36)
+
+    assert_equal(res["B"]["vegetarians"],1)
+    assert_equal(res["B"]["gluten free"],7)
+    assert_equal(res["B"]["other"],2)
+  end
+
 end
